5719cc1f0dad61de79a05f5d8caec72f95b16c43
Akash Piya <akashpiya@gmail.com>
2023-09-28 11:51:56 -0500

    Added fixed celestial objects and trajectories

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,14 @@ use druid::kurbo::{Size, Circle, Line};
 use druid::widget::prelude::*;
 use druid::{
     AppLauncher, LocalizedString, WindowDesc, TimerToken,
-    Color, Point, MouseButton, Code, KeyEvent
+    Color, Point, MouseButton, Code, Modifiers
 };
 
+
 static TIMER_INTERVAL: Duration = Duration::from_millis(10);
+const ALLCOLORS: [Color; 7] = [Color::RED, Color::BLUE, Color::GREEN, Color::PURPLE, Color::YELLOW, Color::WHITE, Color::BLACK];
+const TRAJECTORY_POINTS: u32 = 5;
+
 struct GravityDisplay {
     timer_id: TimerToken,
 }
@@ -27,6 +31,8 @@ struct Simulation {
     scale: f64, 
     camera_pos: Point, 
     init_camera_pos: Point,
+    trajectory: Rc<RefCell<Vec<Point>>>,
+    play: bool,
 }
 
 
@@ -37,6 +43,8 @@ struct CelestialObject {
     mass: f64,
     prev_x: f64,
     prev_y: f64,
+    color: usize,
+    fixed: bool,
 }
 
 
@@ -59,13 +67,33 @@ impl Widget<Simulation> for GravityDisplay {
                 match button.code {
                     Code::Comma => data.scale -= 0.03,
                     Code::Period => data.scale += 0.03,
+                    Code::ArrowRight => {
+                        data.proposed_body.color = (data.proposed_body.color + 1) % ALLCOLORS.len();
+                    },
+                    Code::ArrowLeft => {
+                        if data.proposed_body.color == 0 {
+                            data.proposed_body.color = ALLCOLORS.len() - 1;
+                        } else {
+                            data.proposed_body.color -= 1;
+                        }
+                    },
+                    Code::KeyW => data.camera_pos.y += 1.0 / data.scale * 30.0,
+                    Code::KeyA => data.camera_pos.x += 1.0 / data.scale * 30.0,
+                    Code::KeyS => data.camera_pos.y -= 1.0 / data.scale * 30.0,
+                    Code::KeyD => data.camera_pos.x -= 1.0 / data.scale * 30.0,
+                    Code::Space => data.play = !data.play,
                     _ => {}
                 }
             }
             Event::Timer(id) => {
                 if *id == self.timer_id {
                     ctx.request_paint();
-                    data.update();
+                    if data.play {
+                        data.update();
+                    }
+                    if data.left_mouse_pressed {
+                        data.calculate_trajectory();
+                    }
                     self.timer_id = ctx.request_timer(TIMER_INTERVAL);
                 }
             }
@@ -77,6 +105,7 @@ impl Widget<Simulation> for GravityDisplay {
                         data.init_camera_pos = data.camera_pos.clone();
                     }
                     MouseButton::Left => {
+                        // Normal case if mouse button is pressed
                         data.left_mouse_pressed = true;
                         data.init_cursor_pos = data.cursor_pos.clone();
                     }
@@ -85,7 +114,6 @@ impl Widget<Simulation> for GravityDisplay {
             }
             Event::MouseMove(event) => {
                 data.cursor_pos = event.window_pos.clone();
-                
                 if !data.left_mouse_pressed{
                     data.proposed_body.x = data.cursor_pos.x.clone();
                     data.proposed_body.y = data.cursor_pos.y.clone();
@@ -93,7 +121,7 @@ impl Widget<Simulation> for GravityDisplay {
                 ctx.request_paint();
             }
             Event::Wheel(event) => {
-                data.proposed_body.mass += 0.3 * event.wheel_delta.y;
+                data.proposed_body.mass += 0.5 * event.wheel_delta.y / data.scale;
                 ctx.request_paint();
             }
             Event::MouseUp(mouse) => {
@@ -112,9 +140,13 @@ impl Widget<Simulation> for GravityDisplay {
                         body.y = data.init_cursor_pos.y / data.scale - data.camera_pos.y;
                         body.prev_x = body.x + delta.0 / scale; 
                         body.prev_y = body.y + delta.1 / scale;
+                        if body.color == 6 {
+                            body.fixed = true;
+                        }
                         data.proposed_body = CelestialObject::new(0.0, 0.0, body.mass.clone());
                         data.bodies.borrow_mut().push(body);
                         data.init_cursor_pos = Point::ZERO;
+                        data.trajectory.borrow_mut().clear();
                     }
                     _ => {}
                 }
@@ -123,7 +155,7 @@ impl Widget<Simulation> for GravityDisplay {
             _ => {}
         };
 
-        // // Calculating the camera position
+        //Calculating the camera position
         if data.middle_mouse_pressed {
             let delta = (
                 (data.cursor_pos.x - data.init_cursor_pos.x) / data.scale,
@@ -134,6 +166,9 @@ impl Widget<Simulation> for GravityDisplay {
                 data.init_camera_pos.y + delta.1,
             );
         } 
+
+        // Calculating proposed trajectory
+
     }
     
     fn lifecycle(
@@ -176,24 +211,79 @@ impl Widget<Simulation> for GravityDisplay {
                  (body.y + data.camera_pos.y) * data.scale), 
                 mass_to_radius(body.mass) * data.scale
             );
-            ctx.fill(point, &Color::RED);
+            ctx.fill(point, &ALLCOLORS[body.color]);
         }
         
-        ctx.fill(Circle::new((data.cursor_pos.x, data.cursor_pos.y), mass_to_radius(data.proposed_body.mass) * data.scale), &Color::WHITE);
+        ctx.fill(Circle::new((data.cursor_pos.x, data.cursor_pos.y), mass_to_radius(data.proposed_body.mass) * data.scale), &ALLCOLORS[data.proposed_body.color]);
+
         if data.left_mouse_pressed {
             ctx.stroke(Line::new(data.cursor_pos.clone(), Point::new(data.init_cursor_pos.x, data.init_cursor_pos.y)), &Color::GRAY, 2.5); 
+            
+            if data.trajectory.borrow().len() > 1 {
+                for i in 0..(data.trajectory.borrow().len() - 1){
+                    ctx.stroke(
+                        Line::new(
+                        ((data.trajectory.borrow()[i].x + data.camera_pos.x) * data.scale,
+                        (data.trajectory.borrow()[i].y + data.camera_pos.y) * data.scale),
+                        ((data.trajectory.borrow()[i + 1].x + data.camera_pos.x) * data.scale,
+                        (data.trajectory.borrow()[i + 1].y + data.camera_pos.y) * data.scale)
+                        ),
+                        &Color::GREEN,
+                        3.0
+                    );
+                }
+            }
         }
-
-        println!("{:?}", data.camera_pos);
     }
 }
 
 impl Simulation {
+    // In its current state, the trajectory is calculated as if all the other bodies are stationary.
+    // I think it would be cool to also consider the motion of the other bodies in the trajectory.
+    pub fn calculate_trajectory(&mut self) {
+        let mut body = self.proposed_body.clone();
+        let scale = 20.0;
+        let delta = ((self.cursor_pos.x - self.init_cursor_pos.x) / 1.0, (self.cursor_pos.y - self.init_cursor_pos.y) / 1.0);
+        body.x = self.init_cursor_pos.x / self.scale  - self.camera_pos.x;
+        body.y = self.init_cursor_pos.y / self.scale - self.camera_pos.y;
+        body.prev_x = body.x + delta.0 / scale; 
+        body.prev_y = body.y + delta.1 / scale;
+        self.trajectory.borrow_mut().clear();
+        self.trajectory.borrow_mut().push(Point::new(body.x, body.y));
+
+        let num_points = 500;
+
+        for num in 0..TRAJECTORY_POINTS * num_points {
+            let mut net_force: (f64, f64) = (0.0, 0.0);
+            let mut state = false;
+            for i in 0..self.bodies.borrow().len() {
+                let dist = ((self.bodies.borrow()[i].x - body.x).powf(2.0) + (self.bodies.borrow()[i].y - body.y).powf(2.0)).powf(0.5);
+                if dist <= mass_to_radius(self.bodies.borrow()[i].mass) + mass_to_radius(body.mass) {
+                    state = true;
+                }
+                let result = body.calculate_force(&self.bodies.borrow()[i]);
+                net_force.0 += result.0;
+                net_force.1 += result.1;
+            }
+
+            if state {
+                break;
+            }
+            net_force.0 /= body.mass;
+            net_force.1 /= body.mass;
+            body.update_fields_from_force(&net_force, &self.dt);
+            if num % 1 == 0 {
+                self.trajectory.borrow_mut().push(Point::new(body.x, body.y));
+            }
+        }
+    }
     pub fn update(&mut self) {
         let mut accs: Vec<(f64, f64)> = Vec::new();
         let mut merges: Vec<(usize, usize)> = Vec::new();
+
         for i in 0..self.bodies.borrow().len() {
             let mut net_force = (0.0, 0.0);
+
 
             for j in 0..self.bodies.borrow().len() {
                 //Checking for merges
@@ -214,7 +304,11 @@ impl Simulation {
             net_force.0 /= self.bodies.borrow()[i].mass;
             net_force.1 /= self.bodies.borrow()[i].mass;
 
-            accs.push(net_force);
+            if self.bodies.borrow()[i].fixed {
+                accs.push((0.0, 0.0));
+            } else {
+                accs.push(net_force);
+            }
         }
 
         for (i, body) in self.bodies.borrow_mut().iter_mut().enumerate() {
@@ -238,20 +332,37 @@ impl Simulation {
             );
 
             let index;
+            let color;
             if body1.mass >= body2.mass {
                 index = merges[i].0.clone();
+                color = body1.color.clone();
                 delete_idxs.push(merges[i].1);
             } else {
                 index = merges[i].1.clone();
+                color = body2.color.clone();
                 delete_idxs.push(merges[i].0);
             }
 
-            self.bodies.borrow_mut()[index] = CelestialObject {
-                x: center.0,
-                y: center.1,
-                mass: body1.mass + body2.mass,
-                prev_x: prev_center.0,
-                prev_y: prev_center.1,
+            if body1.fixed || body2.fixed {
+                self.bodies.borrow_mut()[index] = CelestialObject {
+                    x: center.0,
+                    y: center.1,
+                    mass: body1.mass + body2.mass,
+                    prev_x: center.0,
+                    prev_y: center.1,
+                    color: 6,
+                    fixed: true,
+                }
+            } else {
+                self.bodies.borrow_mut()[index] = CelestialObject {
+                    x: center.0,
+                    y: center.1,
+                    mass: body1.mass + body2.mass,
+                    prev_x: prev_center.0,
+                    prev_y: prev_center.1,
+                    color: color,
+                    fixed: false
+                }
             }
         }
 
@@ -274,6 +385,8 @@ impl Simulation {
             scale: 1.0,
             init_cursor_pos: Point::new(0.0, 0.0),
             init_camera_pos: Point::new(0.0, 0.0),
+            trajectory: Rc::new(RefCell::new(Vec::<Point>::new())),
+            play: true,
         }
     }
 }
@@ -307,6 +420,8 @@ impl CelestialObject {
             mass,
             prev_x: x,
             prev_y: y,
+            color: 0 as usize,
+            fixed: false,
         }
     }
 
@@ -316,7 +431,9 @@ impl CelestialObject {
             y: y + v_y,
             mass,
             prev_x: x,
-            prev_y: y
+            prev_y: y,
+            color: 0 as usize,
+            fixed: false,
         }
     }
 }
@@ -330,7 +447,7 @@ fn main(){
 
     let sim = Simulation::new(
         Rc::new(RefCell::new(bodies)),
-        0.05,
+        0.1,
     ) ;
 
     let window = WindowDesc::new(GravityDisplay {timer_id : TimerToken::INVALID}).title(
